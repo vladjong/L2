c67044ea5f4571ae954fa59eb59442419b32ba5a
vladjong <va.gaidenko@gmail.com>
2022-07-30 15:51:20 +0700

    describe ex9

--- ex9/ex9.go
+++ ex9/ex9.go
@@ -9,19 +9,31 @@ import (
 )
 
 func downloadFile(filepath string, url string) error {
+	/*
+		Создание файла
+	*/
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
+	/*
+		Получить данные по ссылке
+	*/
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+	/*
+		Проверка получение данных клиентом
+	*/
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("bad status: %s", resp.Status)
 	}
+	/*
+		Запись страницы в файл
+	*/
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		return err
@@ -35,5 +47,8 @@ func main() {
 		fmt.Println("unable to resolve address")
 	}
 	strs := strings.Split(argsWithoutProg[0], "/")
-	downloadFile(strs[len(strs)-1], argsWithoutProg[0])
+	err := downloadFile(strs[len(strs)-1], argsWithoutProg[0])
+	if err != nil {
+		fmt.Println(err)
+	}
 }
